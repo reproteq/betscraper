37dfbbe8f9fa973f3bbc784fcca212a9b96e3726
Alex Gregori Tomás <reproteq@gmail.com>
2020-02-24 08:51:47 -0800

    Add files via upload

---
+++ wsb724/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace wsb724
+{
+    class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Bet History Scraper");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());            
+
+        }
+    }
+}
+
+
+
